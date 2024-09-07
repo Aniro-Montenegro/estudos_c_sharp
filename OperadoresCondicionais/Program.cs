c3d266bf2e04b1a73b7dcebe311171337e081e7b
Aniro Montenegro <aniro.montenegro@anovasistemas.com.br>
2024-09-07 18:46:56 -0300

    Operadores

---
+++ OperadoresCondicionais/Program.cs
@@ -0,0 +1,49 @@
+﻿
+Console.WriteLine("Digite a sua idade:");
+
+int idade = int.Parse( Console.ReadLine());
+
+if (idade >= 65)
+{
+    Console.WriteLine("Você deve aposentar-se.");
+}
+else if(idade >= 18 && idade < 65)
+{
+    Console.WriteLine("Você é maior de idade.");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade.");
+}
+Console.WriteLine("Qual time de futebol em SP voce torce?");
+Console.WriteLine("1 - Palmeiras");
+Console.WriteLine("2 - Corinthians");
+Console.WriteLine("3 - São Paulo");
+Console.WriteLine("4 - Santos");
+Console.WriteLine("5 - Outro");
+
+int time = int.Parse(Console.ReadLine());
+
+switch (time)
+{
+    case 1:
+        Console.WriteLine("Então voce é palmerense.");
+        break;
+    case 2:
+        Console.WriteLine("Então voce é corinthiano.");
+        break;
+    case 3:
+        Console.WriteLine("Então voce é são paulino.");
+        break;
+    case 4:
+
+        Console.WriteLine("Então voce é santista.");
+        break;
+    case 5:
+        Console.WriteLine("Então voce torce para outro time.");
+        break;
+    default:
+        Console.WriteLine("Opção inválida.");
+        break;
+        
+}
---
+++ OperadoresLogicos/Program.cs
@@ -0,0 +1,53 @@
+﻿
+Console.WriteLine("Digite a sua idade");
+int idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o codigo do produto:");
+int codigo = int.Parse(Console.ReadLine());
+
+Console.WriteLine("1 - Carro");
+Console.WriteLine("2 - Moto");
+Console.WriteLine("3 - Celular");
+Console.WriteLine("4 - Notebook");
+
+Console.WriteLine("Digite a quantidade de dinheiro disponivel:");
+double dinheiro = double.Parse(Console.ReadLine());
+
+if(codigo == 1 ){
+    if(idade >= 18){
+        if(dinheiro >= 50000){
+            Console.WriteLine("Compra efetuada com sucesso");
+        }else{
+            Console.WriteLine("Dinheiro insuficiente");
+        }
+    }else{
+        Console.WriteLine("Compra proibida para menores de 18 anos");
+    }
+}
+else if(codigo ==2){
+    if(idade >=18){
+        if(dinheiro >= 15000){
+            Console.WriteLine("Compra efetuada com sucesso");
+        }else{
+            Console.WriteLine("Dinheiro insuficiente");
+        }
+    }
+}
+else if(codigo == 3){
+    if(dinheiro >= 2000 && idade >= 18){
+        Console.WriteLine("Compra efetuada com sucesso");
+    }else if(dinheiro < 2000 || idade < 18){
+        Console.WriteLine("Dinheiro insuficiente ou idade menor que 18 anos");
+    }
+}
+else if(codigo == 4){
+    if(dinheiro >= 4500 && idade >= 18){
+        Console.WriteLine("Compra efetuada com sucesso");
+    } else if(!(dinheiro > 4500 || idade > 18)){
+        Console.WriteLine("Dinheiro insuficiente ou idade menor que 18 anos");
+    }
+}
+else{
+    Console.WriteLine("Codigo invalido");
+}
+
