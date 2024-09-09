94963e4fb3ceb5689fc4134f100b92f4605628e5
Aniro Montenegro <aniro.montenegro@anovasistemas.com.br>
2024-09-09 08:51:40 -0300

    Operadores Aritmeticos

---
+++ OperadoresAritmeticosd/Program.cs
@@ -0,0 +1,115 @@
+﻿//Adicao
+Console.WriteLine("Adicao");
+Console.WriteLine(2 + 2);
+Console.WriteLine(2.5 + 2.5);
+Console.WriteLine(2 + 2.5);
+
+//Subtracao
+Console.WriteLine("Subtracao");
+Console.WriteLine(2 - 2);
+Console.WriteLine(2.5 - 2.5);
+Console.WriteLine(2 - 2.5);
+
+//Multiplicacao
+Console.WriteLine("Multiplicacao");
+Console.WriteLine(2 * 2);
+Console.WriteLine(2.5 * 2.5);
+Console.WriteLine(2 * 2.5);
+
+//Divisao
+Console.WriteLine("Divisao");
+Console.WriteLine(2 / 2);
+Console.WriteLine(2.5 / 2.5);
+Console.WriteLine(2 / 2.5);
+
+//Resto
+Console.WriteLine("Resto");
+Console.WriteLine(2 % 2);
+Console.WriteLine(2.5 % 2.5);
+Console.WriteLine(2 % 2.5);
+
+//Exponenciacao
+
+Console.WriteLine("Exponenciacao");
+Console.WriteLine(Math.Pow(2, 2));
+Console.WriteLine(Math.Pow(3, 3));
+Console.WriteLine(Math.Pow(10,2));
+
+//Raiz Quadrada
+
+Console.WriteLine("Raiz Quadrada");
+Console.WriteLine(Math.Sqrt(4));
+Console.WriteLine(Math.Sqrt(9));
+Console.WriteLine(Math.Sqrt(100));
+
+//Raiz Cubica
+
+Console.WriteLine("Raiz Cubica");
+Console.WriteLine(Math.Cbrt(8));
+Console.WriteLine(Math.Cbrt(27));
+Console.WriteLine(Math.Cbrt(1000));
+
+//Valor Absoluto
+
+Console.WriteLine("Valor Absoluto");
+Console.WriteLine(Math.Abs(-10));
+Console.WriteLine(Math.Abs(10));
+Console.WriteLine(Math.Abs(-10.5));
+
+
+//Arredondamento
+
+Console.WriteLine("Arredondamento");
+Console.WriteLine(Math.Round(2.7));
+Console.WriteLine(Math.Round(2.4));
+Console.WriteLine(Math.Round(2.5));
+
+//Arredondamento para cima
+
+Console.WriteLine("Arredondamento para cima");
+Console.WriteLine(Math.Ceiling(2.7));
+Console.WriteLine(Math.Ceiling(2.4));
+Console.WriteLine(Math.Ceiling(2.5));
+
+//Arredondamento para baixo
+
+Console.WriteLine("Arredondamento para baixo");
+Console.WriteLine(Math.Floor(2.7));
+Console.WriteLine(Math.Floor(2.4));
+Console.WriteLine(Math.Floor(2.5));
+
+//Incremento
+
+Console.WriteLine("Incremento");
+int numero = 10;
+Console.WriteLine(numero);
+numero++;
+Console.WriteLine(numero);
+
+//Decremento
+
+Console.WriteLine("Decremento");
+Console.WriteLine(numero);
+numero--;
+Console.WriteLine(numero);
+
+
+//seno
+Console.WriteLine("Seno");
+Console.WriteLine(Math.Round(Math.Sin(30), 2));
+Console.WriteLine(Math.Round(Math.Sin(45), 4));
+Console.WriteLine(Math.Round(Math.Sin(90), 4));
+
+//Cosseno
+
+Console.WriteLine("Cosseno");
+Console.WriteLine(Math.Round(Math.Cos(30), 2));
+Console.WriteLine(Math.Round(Math.Cos(45), 4));
+Console.WriteLine(Math.Round(Math.Cos(90), 4));
+
+//Tangente
+
+Console.WriteLine("Tangente");
+Console.WriteLine(Math.Round(Math.Tan(30), 2));
+Console.WriteLine(Math.Round(Math.Tan(45), 4));
+Console.WriteLine(Math.Round(Math.Tan(90), 4));
