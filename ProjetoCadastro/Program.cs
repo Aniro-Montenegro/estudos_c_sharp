2d49c797a5f6e2df050a70a99bb0d7182689bf1e
Aniro Montenegro <aniro.montenegro@anovasistemas.com.br>
2026-07-30 10:25:37 -0300

    atualizacao

---
+++ ProjetoCadastro/Program.cs
@@ -0,0 +1,27 @@
+﻿string opcao;
+bool exibirmenu = true;
+
+while (exibirmenu)
+{
+    Console.WriteLine("1 - Cadastrar");
+    Console.WriteLine("2 - Listar");
+    Console.WriteLine("3 - Sair");
+    Console.WriteLine("Digite a opção desejada: ");
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastrar");
+            break;
+        case "2":
+            Console.WriteLine("Listar");
+            break;
+        case "3":
+            exibirmenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
