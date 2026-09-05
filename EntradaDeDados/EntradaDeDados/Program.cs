19f710dda2c623d7b8fc0b1efa0a3f43ff2f1b71
Aniro Montenegro <aniro.montenegro@anovasistemas.com.br>
2026-09-05 18:48:28 -0300

    esntrada de dados

---
+++ EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+namespace EntradaDados{
+    class Program{
+        static void Main(string[] args){
+            string frase = Console.ReadLine();
+            Console.WriteLine(frase);
+        }
+    }
+}
+
